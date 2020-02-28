4d1ea41ab124919a12c977e8b7830575a1e25d8a
Charbel <charbel.alhelou@ensta-paristech.fr>
2020-02-28 17:00:53 +0100

    debut class

--- parking.h
+++ parking.h
@@ -2,4 +2,23 @@
 #define PARKING_H
 #include <queue.h>
 
+class Parking
+{
+private:
+    unsigned int nb_queues;
+    std::vector< Queue > queues;
+public:
+    Parking(/* args */);
+    ~Parking();
+};
+
+Parking::Parking(/* args */)
+{
+}
+
+Parking::~Parking()
+{
+}
+
 #endif // PARKING_H
+
