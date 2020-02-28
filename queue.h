9289dd8a4cd4f40911b6b466394221d08c0da662
Myriam03 <myriam.hamrouni@esnta-paris.fr>
2020-02-28 16:50:21 +0100

    ajout des déclarations

--- queue.h
+++ queue.h
@@ -10,9 +10,23 @@ class Queue
     int length_file ;
     std::vector<Vehicle> vehicles ;
     std::string file_ID ;
-    vehicle vehicule_in_charge ;
-    std::vector<float> power ;
+    Vehicle vehicule_in_charge ;
+    std::vector<float> powers ;
     
+
+    public :
+    int get_length_file();
+    void set_length_file(int);
+    std::vector<Vehicle> get_vehicles ();
+    void set_vehicles(Vehicle);
+    std::string get_file_ID();
+    void set_file_ID(std::string);
+    Vehicle get_vehicle_in_charge();
+    void set_vehicle_in_charge(Vehicle);
+    std::vector<float> get_powers();
+    void set_powers(float);
+
+
 
 
    
