233bfa638de8835b809513578622d97906f9a893
Myriam03 <myriam.hamrouni@esnta-paris.fr>
2020-02-28 16:31:12 +0100

    version1

--- queue.h
+++ queue.h
@@ -2,4 +2,21 @@
 #define QUEUE_H
 #include <vehicle.h>
 
+
+class queue
+{
+
+    private :
+    int length_file ;
+    std::vector<vehicle> vehicles ;
+    std::string file_ID ;
+    vehicle vehicule_in_charge ;
+    std::vector<float> power ;
+    
+
+
+   
+}
+
+
 #endif // QUEUE_H
